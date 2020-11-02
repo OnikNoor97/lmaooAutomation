de93e041e8cdbc67a337403fff1773cc87e8c40a
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-11-02 18:40:14 +0000

    Started Testing of NavBar

---
+++ TestRepository/Feature/NavBar.feature
@@ -0,0 +1,6 @@
+﻿Feature: NavBar
+	This Feature file will be used for navigation actions
+
+@Navbar
+Scenario: I navigate to all pages
+	And I navigate to the Home Page
---
+++ TestRepository/Steps/NavBarSteps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace TestRepository.Steps
+{
+    [Binding]
+    public class NavBarSteps
+    {
+        [Given(@"I navigate to the Home Page")]
+        public void GivenINavigateToTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
